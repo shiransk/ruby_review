10e44357661a0da2eaedd28fccec39563902c64d
Shiran Koriat <shiransk@gmail.com>
2016-07-11 20:06:36 -0700

    first init

---
+++ exmple.rb
@@ -0,0 +1,3 @@
+food = {animal: "dog" , man: "old"} #this is the new way of writing code .
+
+
---
+++ store_item.rb
@@ -0,0 +1,8 @@
+#pet store
+
+pet1 = {breed: "dog" , age: 7 , name: "wafwaf" }
+pet2 = {breed: "cat" , age: 12 , name: "meowmewo"}
+pet3 = {:breed => "snake" , :age => 55 , :name => "pssssss"}
+
+
+

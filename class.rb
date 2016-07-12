a798cd2bf28db638dfc58cef13246277a88b4743
Shiran Koriat <shiransk@gmail.com>
2016-07-11 23:06:33 -0700

    moving notes.rb to main folder

---
+++ class.rb
@@ -0,0 +1,30 @@
+class Employee
+
+  def initialize(employe_hash)
+    @first_name = employe_hash[:first_name]
+    @last_name = employe_hash[:last_name]
+    @salary = employe_hash[:salary]
+    @active = true
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def set_first_name(new_name)
+    @first_name = new_name
+  end
+
+
+end
+
+employe1 = Employee.new({last_name: "Bob" ,first_name: "Barker" ,salary: 100000})
+
+puts employe1.first_name
+
+
+# puts employe1.first_name
+
+# employe1.set_first_name("sally")
+
+# puts employe1.first_name
--- notes.rb
+++
@@ -1,9 +0,0 @@
-# git-hub push : git push origin master
-
-food = {animal: "dog" , man: "old"} #this is the new way of writing code .
-
-# :a = symbole , a: = symbole + => 
-
-
-
-
